1fb8b2f82d1f50869e06de041c9ba001119a8ca4
VadimAlek <106871205+VadimAlek@users.noreply.github.com>
2022-08-25 07:19:16 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprolist/SkyproListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproListApplication.class, args);
+	}
+
+}
